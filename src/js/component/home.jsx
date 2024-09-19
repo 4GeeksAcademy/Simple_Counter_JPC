11efd0e9f83e361fd65f3ef137ee0be6ec934257
Jumpvzl <40275118+Jumpvzl@users.noreply.github.com>
2024-09-19 20:00:20 +0000

    se sube el contenido

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import Footer from "./footer.jsx";
+import Card from "./card.jsx";
+import SecondsCounter from "./SecondsCounter.jsx";
 //create your first component
+
 const Home = () => {
+
+	const footerData = [
+		{
+			githubURL: "https://github.com/Jumpvzl",
+			name: "Juan Pablo Castillo",
+			geeksName: "4geeksacademy",
+			geeksURL: "https://www.4geeksacademy.com",
+			imagenURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.NzJUc0PYtkgp7lfNKizmgQHaHB%26pid%3DApi&f=1&ipt=271cb3eb7e5f15522c250313a63e781bdb17fd428148a3cfa3fc136c7c118f42&ipo=images",
+		},
+	];
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+		<div style={{ display: 'flex', flexDirection: 'column', minHeight: '90vh' }}>
+			<div style={{ flex: 1 }}>
+				<SecondsCounter />
+			</div>
+			
+			
+			</div>
+			<Footer properties={footerData} />
 		</div>
+		
 	);
 };
 
